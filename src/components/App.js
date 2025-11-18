8e59b707abcd9e04fdecaedfcb5f90a05e47e022
nithinbairu <nithinbairu08@gmail.com>
2025-11-18 10:36:16 +0530

    1st commit

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,28 @@
 
 import React from "react";
 import './../styles/App.css';
-
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import { Link } from "react-router-dom/cjs/react-router-dom.min";
+import Home from "./Home";
+import About from "./About";
 const App = () => {
   return (
-    <div>
-        {/* Do not remove the main div */}
-    </div>
+    <Router>
+      <nav>
+        <ul>
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/about">About</Link>
+          </li>
+        </ul>
+      </nav>
+      <Switch>
+        <Route exact path="/" component={Home} />
+        <Route path="/about" component={About} />
+      </Switch>
+    </Router>
   )
 }
 
